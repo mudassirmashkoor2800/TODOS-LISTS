d35de48e3b08995aa00f5841efb73b93f4e0e4a7
mudassirmashkoor2800 <mudassirmashkoor@gmail.com>
2024-04-01 20:27:42 +0500

    todos-list

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todos = [];
+
+let condition = true;
+
+while(condition)
+{
+    let addTask = await inquirer.prompt([{
+        name : "todos",
+        type : "input",
+        message : "What do you want to add in ToDos List?",
+    },
+    {
+        name : "addMore",
+        type : "confirm",
+        message : "Do you want to add more task?",
+        default : "false",
+    }
+    ]);
+    todos.push(addTask.todos);
+    condition = addTask.addMore;
+    console.log(todos);
+    
+    
+}
